9b9764dae8eaa1ab7bca3fbb3be8530bcc1659f7
ericanthonywu <ericanthonywu89@gmail.com>
2020-07-14 17:41:40 +0700

    add email at user

--- model.js
+++ model.js
@@ -21,6 +21,7 @@ exports.admin = mongoose.model("admin", adminSchema);
 
 const userSchema = new mongoose.Schema({
     username: {type: String, required: true, trim: true, unique: true},
+    email: {type: String, unique: true},
     password: {type: String, required: true, select: false},
     cart: [{
         productId: {type: mongoose.Schema.Types.ObjectID, ref: 'product'},
