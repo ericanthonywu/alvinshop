b26254084c8053efdfea02815bf42604944f5dce
ericanthony <ericanthonywu89@gmail.com>
2020-12-19 21:25:58 +0700

    add static uploads

--- app.js
+++ app.js
@@ -22,12 +22,10 @@ app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
-const usersRouter = require('./routes/users');
-const adminRouter = require('./routes/admin');
-
-app.use('/user', usersRouter);
-app.use('/admin', adminRouter);
+app.use('/user', require('./routes/users'));
+app.use('/admin', require('./routes/admin'));
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
